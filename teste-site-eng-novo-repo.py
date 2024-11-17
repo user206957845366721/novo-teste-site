3fdaebe885e50866ea1349a1f678f08f75e815ea
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-17 15:12:03 +0000

    Update teste-site-eng-novo-repo.py

--- teste-site-eng-novo-repo.py
+++ teste-site-eng-novo-repo.py
@@ -1,5 +1,3 @@
-Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 import streamlit as st
 import pandas as pd
 import numpy as np
